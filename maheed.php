c7cb996eec48579555e1954422536f14699e9f42
Ian Hiew <pixdo.et@gmail.com>
2022-02-15 12:07:30 +0800

    THE HOLY FILE HAS SPOKEN

--- maheed.php
+++ maheed.php
@@ -3,5 +3,5 @@ include('includes/youtubei/createRequest.php');
 
 header("Content-type: application/json");
 
-$k = requestChannel("UC0ifhOnVBCUkpS-_FhcgQEQ","about");
+$k = requestChannel("UCt_VTKy-LTj-49tYxQw9kDA","about");
 echo $k;
