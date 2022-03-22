ee14286ee2470bf9f627f3dd3d037a9904f87617
Ian Hiew <pixdo.et@gmail.com>
2022-03-22 14:42:32 +0800

    Trimmed and debloated createRequest.php

--- includes/youtubei/trimmedRequest.php
+++ includes/youtubei/trimmedRequest.php
@@ -13,153 +13,16 @@ function requestBrowse($brid)
             array(
                 'client' =>
                 array(
-                    'hl' => 'en',
-                    'gl' => 'MY',
-                    'remoteHost' => '2001:e68:5418:cf5d:9daa:1106:dd70:103e',
-                    'deviceMake' => 'Apple',
-                    'deviceModel' => '',
-                    'visitorData' => 'Cgtjc1hsUzJrS2tlWSiBnYaNBg%3D%3D',
+                    'visitorData' => 'Cgtjc1hsUzJrS2tlWSjImoiNBg%3D%3D',
                     'userAgent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:94.0) Gecko/20100101 Firefox/94.0,gzip(gfe)',
                     'clientName' => 'WEB',
                     'clientVersion' => '2.20211124.00.00',
-                    'osName' => 'Macintosh',
-                    'osVersion' => '10.14',
                     'originalUrl' => 'https://www.youtube.com/feed/explore',
-                    'platform' => 'DESKTOP',
-                    'clientFormFactor' => 'UNKNOWN_FORM_FACTOR',
-                    'configInfo' =>
-                    array(
-                        'appInstallData' => 'CIGdho0GELDUrQUQl-qtBRCT6q0FEJLXrQUQt8utBRCU_vwSENi-rQUQkfj8Eg%3D%3D',
-                    ),
-                    'userInterfaceTheme' => 'USER_INTERFACE_THEME_DARK',
-                    'timeZone' => 'Asia/Kuala_Lumpur',
-                    'browserName' => 'Firefox',
-                    'browserVersion' => '94.0',
-                    'screenWidthPoints' => 1438,
-                    'screenHeightPoints' => 889,
-                    'screenPixelDensity' => 1,
-                    'screenDensityFloat' => 1,
-                    'utcOffsetMinutes' => 480,
-                    'mainAppWebInfo' =>
-                    array(
-                        'graftUrl' => '/feed/subscriptions',
-                        'webDisplayMode' => 'WEB_DISPLAY_MODE_BROWSER',
-                        'isWebNativeShareAvailable' => false,
-                    ),
-                ),
-                'user' =>
-                array(
-                    'lockedSafetyMode' => false,
-                ),
-                'request' =>
-                array(
-                    'useSsl' => true,
-                    'internalExperimentFlags' =>
-                    array(),
-                    'consistencyTokenJars' =>
-                    array(),
-                ),
-                'clickTracking' =>
-                array(
-                    'clickTrackingParams' => 'CGUQtSwYAiITCKzE2sa2t_QCFcj8OAYdOUsJuA==',
-                ),
-                'adSignalsInfo' =>
-                array(
-                    'params' =>
-                    array(
-                        0 =>
-                        array(
-                            'key' => 'dt',
-                            'value' => '1637977730876',
-                        ),
-                        1 =>
-                        array(
-                            'key' => 'flash',
-                            'value' => '0',
-                        ),
-                        2 =>
-                        array(
-                            'key' => 'frm',
-                            'value' => '0',
-                        ),
-                        3 =>
-                        array(
-                            'key' => 'u_tz',
-                            'value' => '480',
-                        ),
-                        4 =>
-                        array(
-                            'key' => 'u_his',
-                            'value' => '1',
-                        ),
-                        5 =>
-                        array(
-                            'key' => 'u_h',
-                            'value' => '1080',
-                        ),
-                        6 =>
-                        array(
-                            'key' => 'u_w',
-                            'value' => '1920',
-                        ),
-                        7 =>
-                        array(
-                            'key' => 'u_ah',
-                            'value' => '974',
-                        ),
-                        8 =>
-                        array(
-                            'key' => 'u_aw',
-                            'value' => '1920',
-                        ),
-                        9 =>
-                        array(
-                            'key' => 'u_cd',
-                            'value' => '24',
-                        ),
-                        10 =>
-                        array(
-                            'key' => 'bc',
-                            'value' => '31',
-                        ),
-                        11 =>
-                        array(
-                            'key' => 'bih',
-                            'value' => '889',
-                        ),
-                        12 =>
-                        array(
-                            'key' => 'biw',
-                            'value' => '1423',
-                        ),
-                        13 =>
-                        array(
-                            'key' => 'brdim',
-                            'value' => '0,23,0,23,1920,23,1920,974,1438,889',
-                        ),
-                        14 =>
-                        array(
-                            'key' => 'vis',
-                            'value' => '1',
-                        ),
-                        15 =>
-                        array(
-                            'key' => 'wgl',
-                            'value' => 'true',
-                        ),
-                        16 =>
-                        array(
-                            'key' => 'ca_type',
-                            'value' => 'image',
-                        ),
-                    ),
                 ),
             ),
             'browseId' => $brid,
         )
     );
-
-    // echo $brid;  // left this in oops
     $ch = curl_init();
     // otherwise unsupported browser
     $ua = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:100.0) Gecko/20100101 Firefox/100.0";
@@ -191,6 +54,7 @@ function requestPlayer($videoId)
                 'client' =>
                 array(
                     'visitorData' => 'Cgtjc1hsUzJrS2tlWSjImoiNBg%3D%3D',
+                    'clientName' => 'WEB',
                     'userAgent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:94.0) Gecko/20100101 Firefox/94.0,gzip(gfe)',
                     'clientVersion' => '2.20211124.00.00',
                     'originalUrl' => 'https://www.youtube.com/watch?v=' . $videoId,
@@ -210,7 +74,7 @@ function requestPlayer($videoId)
     $ch = curl_init();
     // otherwise unsupported browser
     $ua = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:100.0) Gecko/20100101 Firefox/100.0";
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // so that the 1 doesnt show
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_HTTPHEADER, array(
         "Authorization: ", //sapisidhash,
         "Content-Type: application/json",
@@ -255,146 +119,11 @@ function requestChannel($channelId, $type)
             array(
                 'client' =>
                 array(
-                    'hl' => 'en',
-                    'gl' => 'MY',
-                    'remoteHost' => '2001:e68:5418:cf5d:9daa:1106:dd70:103e',
-                    'deviceMake' => 'Apple',
-                    'deviceModel' => '',
                     'visitorData' => 'Cgtjc1hsUzJrS2tlWSiBnYaNBg%3D%3D',
-                    'userAgent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:94.0) Gecko/20100101 Firefox/94.0,gzip(gfe)',
                     'clientName' => 'WEB',
+                    'userAgent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:94.0) Gecko/20100101 Firefox/94.0,gzip(gfe)',
                     'clientVersion' => '2.20211124.00.00',
-                    'osName' => 'Macintosh',
-                    'osVersion' => '10.14',
                     'originalUrl' => 'https://www.youtube.com/feed/explore',
-                    'platform' => 'DESKTOP',
-                    'clientFormFactor' => 'UNKNOWN_FORM_FACTOR',
-                    'configInfo' =>
-                    array(
-                        'appInstallData' => 'CIGdho0GELDUrQUQl-qtBRCT6q0FEJLXrQUQt8utBRCU_vwSENi-rQUQkfj8Eg%3D%3D',
-                    ),
-                    'userInterfaceTheme' => 'USER_INTERFACE_THEME_DARK',
-                    'timeZone' => 'Asia/Kuala_Lumpur',
-                    'browserName' => 'Firefox',
-                    'browserVersion' => '94.0',
-                    'screenWidthPoints' => 1438,
-                    'screenHeightPoints' => 889,
-                    'screenPixelDensity' => 1,
-                    'screenDensityFloat' => 1,
-                    'utcOffsetMinutes' => 480,
-                    'mainAppWebInfo' =>
-                    array(
-                        'graftUrl' => '/feed/subscriptions',
-                        'webDisplayMode' => 'WEB_DISPLAY_MODE_BROWSER',
-                        'isWebNativeShareAvailable' => false,
-                    ),
-                ),
-                'user' =>
-                array(
-                    'lockedSafetyMode' => false,
-                ),
-                'request' =>
-                array(
-                    'useSsl' => true,
-                    'internalExperimentFlags' =>
-                    array(),
-                    'consistencyTokenJars' =>
-                    array(),
-                ),
-                'clickTracking' =>
-                array(
-                    'clickTrackingParams' => 'CGUQtSwYAiITCKzE2sa2t_QCFcj8OAYdOUsJuA==',
-                ),
-                'adSignalsInfo' =>
-                array(
-                    'params' =>
-                    array(
-                        0 =>
-                        array(
-                            'key' => 'dt',
-                            'value' => '1637977730876',
-                        ),
-                        1 =>
-                        array(
-                            'key' => 'flash',
-                            'value' => '0',
-                        ),
-                        2 =>
-                        array(
-                            'key' => 'frm',
-                            'value' => '0',
-                        ),
-                        3 =>
-                        array(
-                            'key' => 'u_tz',
-                            'value' => '480',
-                        ),
-                        4 =>
-                        array(
-                            'key' => 'u_his',
-                            'value' => '1',
-                        ),
-                        5 =>
-                        array(
-                            'key' => 'u_h',
-                            'value' => '1080',
-                        ),
-                        6 =>
-                        array(
-                            'key' => 'u_w',
-                            'value' => '1920',
-                        ),
-                        7 =>
-                        array(
-                            'key' => 'u_ah',
-                            'value' => '974',
-                        ),
-                        8 =>
-                        array(
-                            'key' => 'u_aw',
-                            'value' => '1920',
-                        ),
-                        9 =>
-                        array(
-                            'key' => 'u_cd',
-                            'value' => '24',
-                        ),
-                        10 =>
-                        array(
-                            'key' => 'bc',
-                            'value' => '31',
-                        ),
-                        11 =>
-                        array(
-                            'key' => 'bih',
-                            'value' => '889',
-                        ),
-                        12 =>
-                        array(
-                            'key' => 'biw',
-                            'value' => '1423',
-                        ),
-                        13 =>
-                        array(
-                            'key' => 'brdim',
-                            'value' => '0,23,0,23,1920,23,1920,974,1438,889',
-                        ),
-                        14 =>
-                        array(
-                            'key' => 'vis',
-                            'value' => '1',
-                        ),
-                        15 =>
-                        array(
-                            'key' => 'wgl',
-                            'value' => 'true',
-                        ),
-                        16 =>
-                        array(
-                            'key' => 'ca_type',
-                            'value' => 'image',
-                        ),
-                    ),
                 ),
             ),
             'browseId' => $channelId,
@@ -402,11 +131,9 @@ function requestChannel($channelId, $type)
         )
     );
 
-    // echo $brid;  // left this in oops
     $ch = curl_init();
-    // otherwise unsupported browser
     $ua = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:100.0) Gecko/20100101 Firefox/100.0";
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // so that the 1 doesnt show
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_COOKIEFILE, "../../cookies.txt");
     curl_setopt($ch, CURLOPT_HTTPHEADER, array(
         "Authorization: ",
@@ -426,156 +153,18 @@ function requestChannel($channelId, $type)
 function requestSearch($query)
 {
     // endpoint: /youtube1/v1/search
-    /*
-    Search functions using youtubei.
-    Otherwise, the site is basically unusable :him:
-  */
+
     include("includes/config.inc.php");
     $req_arr = json_encode(array(
         'context' =>
         array(
             'client' =>
             array(
-                'hl' => 'en',
-                'gl' => 'MY',
-                'remoteHost' => '180.74.66.22',
-                'deviceMake' => 'Apple',
-                'deviceModel' => '',
                 'visitorData' => 'Cgtjc1hsUzJrS2tlWSj0752NBg%3D%3D',
                 'userAgent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:94.0) Gecko/20100101 Firefox/94.0,gzip(gfe)',
                 'clientName' => 'WEB',
                 'clientVersion' => '2.20211129.09.00',
-                'osName' => 'Macintosh',
-                'osVersion' => '10.14',
                 'originalUrl' => 'https://www.youtube.com/results?search_query=' . $query,
-                'platform' => 'DESKTOP',
-                'clientFormFactor' => 'UNKNOWN_FORM_FACTOR',
-                'configInfo' =>
-                array(
-                    'appInstallData' => 'CPTvnY0GELfLrQUQsNStBRCS160FEJfqrQUQlP78EhDYvq0FEJH4_BI%3D',
-                ),
-                'userInterfaceTheme' => 'USER_INTERFACE_THEME_DARK',
-                'timeZone' => 'Asia/Kuala_Lumpur',
-                'browserName' => 'Firefox',
-                'browserVersion' => '94.0',
-                'screenWidthPoints' => 1345,
-                'screenHeightPoints' => 890,
-                'screenPixelDensity' => 1,
-                'screenDensityFloat' => 1,
-                'utcOffsetMinutes' => 480,
-                'mainAppWebInfo' =>
-                array(
-                    'graftUrl' => '/results?search_query=' . $query,
-                    'webDisplayMode' => 'WEB_DISPLAY_MODE_BROWSER',
-                    'isWebNativeShareAvailable' => false,
-                ),
-            ),
-            'user' =>
-            array(
-                'lockedSafetyMode' => false,
-            ),
-            'request' =>
-            array(
-                'useSsl' => true,
-                'internalExperimentFlags' =>
-                array(),
-                'consistencyTokenJars' =>
-                array(),
-            ),
-            'clickTracking' =>
-            array(
-                'clickTrackingParams' => 'CB0Q7VAiEwimvoXx2cL0AhXcQQ8CHTmdAE8=',
-            ),
-            'adSignalsInfo' =>
-            array(
-                'params' =>
-                array(
-                    0 =>
-                    array(
-                        'key' => 'dt',
-                        'value' => '1638365174421',
-                    ),
-                    1 =>
-                    array(
-                        'key' => 'flash',
-                        'value' => '0',
-                    ),
-                    2 =>
-                    array(
-                        'key' => 'frm',
-                        'value' => '0',
-                    ),
-                    3 =>
-                    array(
-                        'key' => 'u_tz',
-                        'value' => '480',
-                    ),
-                    4 =>
-                    array(
-                        'key' => 'u_his',
-                        'value' => '2',
-                    ),
-                    5 =>
-                    array(
-                        'key' => 'u_h',
-                        'value' => '1080',
-                    ),
-                    6 =>
-                    array(
-                        'key' => 'u_w',
-                        'value' => '1920',
-                    ),
-                    7 =>
-                    array(
-                        'key' => 'u_ah',
-                        'value' => '975',
-                    ),
-                    8 =>
-                    array(
-                        'key' => 'u_aw',
-                        'value' => '1920',
-                    ),
-                    9 =>
-                    array(
-                        'key' => 'u_cd',
-                        'value' => '24',
-                    ),
-                    10 =>
-                    array(
-                        'key' => 'bc',
-                        'value' => '31',
-                    ),
-                    11 =>
-                    array(
-                        'key' => 'bih',
-                        'value' => '890',
-                    ),
-                    12 =>
-                    array(
-                        'key' => 'biw',
-                        'value' => '1330',
-                    ),
-                    13 =>
-                    array(
-                        'key' => 'brdim',
-                        'value' => '0,23,0,23,1920,23,1920,975,1345,890',
-                    ),
-                    14 =>
-                    array(
-                        'key' => 'vis',
-                        'value' => '1',
-                    ),
-                    15 =>
-                    array(
-                        'key' => 'wgl',
-                        'value' => 'true',
-                    ),
-                    16 =>
-                    array(
-                        'key' => 'ca_type',
-                        'value' => 'image',
-                    ),
-                ),
             ),
         ),
         'query' => $query,
@@ -583,7 +172,7 @@ function requestSearch($query)
     ));
     $ch = curl_init();
 
-    $ua = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:100.0) Gecko/20100101 Firefox/100.0"; // otherwise unsupported browser
+    $ua = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:100.0) Gecko/20100101 Firefox/100.0";
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // so that the 1 doesnt show
     curl_setopt($ch, CURLOPT_COOKIEFILE, "../../cookies.txt");
     curl_setopt($ch, CURLOPT_HTTPHEADER, array(
